338f6d49b95d54dc27fecf0cd6013579cecee68f
tapnioc <59933584+tapnioc@users.noreply.github.com>
2020-01-22 20:11:37 -0500

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -11,7 +11,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
- * Christian Tapnio
+ * Christian Tapnio - 991359879
  * @author srinivsi
  */
 public class Card {
